ca646fa9f260834409e0542a7f855f93bcd89845
thepizzaking <thepizzaking@252b1adf-bb6a-42e4-b8df-258177e4929f>
2007-07-20 12:49:23 +0000

    Some documentation to the make.py file
    git-svn-id: svn+ssh://svn.gna.org/svn/whaawmp/trunk@176 252b1adf-bb6a-42e4-b8df-258177e4929f

--- make.py
+++ make.py
@@ -21,10 +21,10 @@ import os, sys, os.path
 
 # Directories where to install files from.
 pycDir = [ 'src/common', 'src/gui' ]
-pyDir = [ 'src' ] + pycDir
 gladeDir = 'src/gui'
-binFiles = [ 'whaawmp', 'whaaw-thumbnailer' ]
+execFiles = [ 'whaawmp', 'whaaw-thumbnailer' ]
 
+# Try and read the PREFIX and DESTDIR enivronment variable.
 try:
 	destdir = os.path.abspath(os.environ['DESTDIR'])
 except KeyError:
@@ -34,35 +34,45 @@ try:
 	prefix = os.path.abspath(os.environ['PREFIX'])
 except KeyError:
 	prefix = '/usr/local'
+# Create a base variable where to install all files.
 base = destdir + prefix
 
+# Change directories into the one where this script resides.
 os.chdir(sys.path[0])
 
 if (len(sys.argv) > 1):
+	# If an argument was passed, it will be the target (or help)
 	command = sys.argv[1].lower()
 else:
+	# If none were passed use all.
 	command = 'all'
 
 def makeAll():
+	# Make all, called when no command was passed.
 	compilePy()
 	compilePO()
 	print "Make finished, type './make.py install' to install."
 
 def compilePy():
+	# Compiles all the .py files into .pyc files.
 	print 'Compiling all .py files'
 	check(os.system('python -m compileall src'))
 
 def compilePO():
+	# Compiles all the translations (.po files)
 	print 'Compiling all .po files'
 	check(os.system('./po/potool.py compile'))
 
 def makeInstall():
+	# Installs the program.
 	makeInstallDirs()
 	installBin()
 	installLocales()
 	print '\n\nDone!!!'
 
 def makeInstallDirs():
+	# Makes the install directories.
+	# Only need the end of the trees sinse makedirs is used.
 	dirs = [ '/bin',
 	         '/share/applications',
 	         '/share/thumbnailers',
@@ -74,34 +84,51 @@ def makeInstallDirs():
 		if (not os.path.isdir(base + x)): os.makedirs(base + x)
 
 def installBin():
+	# Installs all the binary files.
+	# First the desktop files to their required locations.
 	install('whaawmp.desktop', '%s/share/applications' % base)
 	install('whaaw-thumbnailer.desktop', '%s/share/thumbnailers' % base)
+	# Images.
 	install('images/*.png', '%s/share/whaawmp/images' % base)
+	# The .py files used to start the program, the .pyc files of these aren't
+	# used anyway.
 	install('src/*.py', '%s/share/whaawmp/src' % base)
 	for x in pycDir:
+		# For all files in the .pyc directories.
 		install('%s/*.pyc' % x, '%s/share/whaawmp/%s' % (base, x))
+	# Now the gladefile too.
 	x = gladeDir
 	install('%s/*.glade' % x, '%s/share/whaawmp/%s' % (base, x))
-	for x in binFiles:
+	for x in execFiles:
+		# For all of them in executable files list.
+		# Install the file as executable
 		install(x, '%s/share/whaawmp' % base, 755)
+		# Open a file in /bin and put a script there.
 		f = open(base + '/bin/%s' % x, 'w')
 		f.write('#!/bin/sh\nexec %s/share/whaawmp/%s "$@"' % (prefix, x))
 		f.close
+		# Set the script to executable.
 		check(os.system('chmod 755 %s/bin/%s' % (base, x)))
 
 def installLocales():
+	# Installs all the locales
 	for x in os.popen('find po -name whaawmp.mo').read().split():
+		# For all locales found, install them to their correct locations.
 		dest = base + '/share/locale/%s' % x[2:]
 		install(x, dest, args='-D')
 	
 
 def makeInstallSrc():
+	# Installs the source (.py) files along with the .pyc files.
 	makeInstall()
-	for x in pyDir:
+	for x in pycDir:
+		# For all the directories we installed the .pyc file in, put the .py
+		# files in there too.
 		install('%s/*.py' % x, '%s/share/whaawmp/%s' % (base, x))
 
 
 def makeUninstall():
+	# Uninstalls all the program (except for locales, I'll get that tomorrow).
 	os.system('rm -r %s/share/whaawmp' % base)
 	os.system('rm %s/share/applications/whaawmp.desktop' % base)
 	os.system('rm %s/share/thumbnailers/whaaw-thumbnailer.desktop' % base)
@@ -109,24 +136,28 @@ def makeUninstall():
 	os.system('rm %s/bin/whaaw-thumbnailer' % base)
 
 def install(src, dst, perm='644', args=''):
+	# Installs a file with the requested attributes (checks for failure)
 	perm = str(perm)
 	print 'Installing %s to %s as %s' % (src, dst, perm)
 	check(os.system('install %s -m %s %s %s' % (args, perm, src, dst)), 'Installation Failed!')
 
 
 def check(x, msg='Fail!'):
+	# Checks if an external command failed, and quits if it did.
 	if (x):
 		print msg
 		sys.exit(1)
 
 
 def printHelp(commands):
+	# Prints a line about usage, then a list of available options.
 	print "Usage: './main.py [option]' where option is one of:"
 	for x in commands:
 		print x
 
 
 def __init__():
+	# A list of commands.
 	commands = { 'all' : makeAll,
 	             'compilepy' : compilePy,
 	             'compilepo' : compilePO,
@@ -136,11 +167,13 @@ def __init__():
 	             'installlocales' : installLocales,
 	             'uninstall' : makeUninstall }
 	
+	# If the command is a help one, print help and quit.
 	if (command in [ '--help', '-h', 'help']):
 		printHelp(commands)
 		sys.exit(0)
 	
 	try:
+		# Try and execute the command, if it fails, quit.
 		commands[command]()
 	except KeyError:
 		print "Could not find target '%s'" % (command)
