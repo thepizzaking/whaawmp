7bf34cdc8a51129b6810d930b8759854ceb76a4a
thepizzaking <thepizzaking@252b1adf-bb6a-42e4-b8df-258177e4929f>
2008-03-11 04:27:28 +0000

    Tag the 0.2.8 releases.
    git-svn-id: svn+ssh://svn.gna.org/svn/whaawmp/trunk@440 252b1adf-bb6a-42e4-b8df-258177e4929f

--- src/common/useful.py
+++ src/common/useful.py
@@ -22,7 +22,7 @@ import os, gobject, sys
 # Nice variables.
 sName = 'whaawmp'
 lName = _('Whaaw! Media Player')
-version = '0.2.7'
+version = '0.2.9'
 origDir = os.getcwd()
 dataDir = '@datadir@'
 if (dataDir == '@' + 'datadir@'): dataDir = os.path.join(sys.path[0], '..')
