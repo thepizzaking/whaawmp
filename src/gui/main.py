c89d7c331fce15d241b75172ede22966122f3514
thepizzaking <thepizzaking@252b1adf-bb6a-42e4-b8df-258177e4929f>
2008-04-01 08:38:23 +0000

    Remove some win32 hacks which sjohannes says no-longer work.
    git-svn-id: svn+ssh://svn.gna.org/svn/whaawmp/trunk@455 252b1adf-bb6a-42e4-b8df-258177e4929f

--- src/gui/main.py
+++ src/gui/main.py
@@ -381,16 +381,6 @@ class mainWindow:
 			player.setURI(file)
 			# Add the file to recently opened files.
 			self.addToRecent(file)
-			
-			# HACK: Force embedding the video into our video window because
-			# onPlayerSyncMessage is never called in Windows.
-			if (sys.platform == 'win32'):
-				class C: pass
-				message = C()
-				message.src = player.player.props.video_sink
-				message.structure = C()
-				message.structure.get_name = lambda: 'prepare-xwindow-id'
-				self.onPlayerSyncMessage(None, message)
 
 			# Start the player.
 			player.play()
@@ -652,7 +642,6 @@ class mainWindow:
 	
 	def drawvideoWindowImage(self):
 		## Draws the background image.
-		if (sys.platform == 'win32'): return
 		# Get the width & height of the videoWindow.
 		alloc = self.videoWindow.get_allocation()
 		w = alloc.width
