3cc53a40826f8f371465c3153d33d457bf40a74c
Jeffrey Bailes <thepizzaking@gmail.com>
2008-12-18 14:12:52 +1100

    Update version for release

--- src/common/useful.py
+++ src/common/useful.py
@@ -29,7 +29,7 @@ import urllib
 # Nice variables.
 sName = 'whaawmp'
 lName = _('Whaaw! Media Player')
-version = '0.2.11'
+version = '0.2.10.1'
 origDir = os.getcwd()
 dataDir = '@datadir@'
 if (dataDir == '@' + 'datadir@'): dataDir = os.path.join(sys.path[0], '..')
