4650eabe4ec8bf8d8ab94f29bd5c8ed8ce65d3ff
Jeffrey Bailes <thepizzaking@gmail.com>
2009-07-24 19:28:49 +1000

    Cleanup

--- src/gui/subtitles.py
+++ src/gui/subtitles.py
@@ -36,25 +36,34 @@ class subMan():
 		self.window.destroy()
 	
 	def autoSubsToggled(self, widget):
+		# Set the automatic subtitles config option.
 		cfg.set('video/autosub', widget.get_active())
 	
 	def subsExtsChanged(self, widget):
+		# Change the automatically detected subtitles extensions.
 		cfg.set('video/autosubexts', widget.get_text())
 	
 	def getCfg(self):
+		# Get the config options and give it to the window.
 		self.chkAutoSubs.set_active(cfg.getBool('video/autosub'))
 		self.txtSubsExt.set_text(cfg.getStr('video/autosubexts'))
 	
 	def addSubs(self, widget):
+		# Add subtitles to the current file from a file dialogue.
 		dlg = gtk.FileChooserDialog(_("Choose a subtitle stream."), self.window,
 		                  buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
 		                             gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+		# Use the same folder as the last file opened.
 		dlg.set_current_folder(useful.lastFolder)
 		res = dlg.run()
+		
 		if (res == gtk.RESPONSE_OK):
+			# If the response was 'OK' get the filename.
 			file = dlg.get_filename()
-			played = player.getPlayed()
+			# We need to restart the player so the subtitles work.
+			#played = player.getPlayed()
 			player.stop()
+			# Reset everything.
 			player.player.set_property('uri', player.uri)
 			player.player.set_property('suburi', useful.filenameToUri(file))
 			player.play()
@@ -63,12 +72,14 @@ class subMan():
 		dlg.destroy()
 	
 	def __init__(self):
+		# The subtitle manager window.
 		window = gtk.Window()
 		window.set_title(_("Subtitle Manager"))
 		window.connect('delete-event', self.destroy)
 		self.window = window
 		vBox = gtk.VBox()
 		window.add(vBox)
+		# The automatic subtitles checkbox.
 		chkAutoSubs = gtk.CheckButton(_("Automatic Subtitles"))
 		chkAutoSubs.set_has_tooltip(True)
 		chkAutoSubs.set_tooltip_text(_("Try to automatically find subtitles for each file."))
@@ -77,6 +88,7 @@ class subMan():
 		vBox.pack_start(chkAutoSubs)
 		hBox = gtk.HBox()
 		vBox.pack_start(hBox)
+		# The automatic subtitle extensions entry.
 		lblSubsExt = gtk.Label(_("Subtitle file extensions"))
 		hBox.pack_start(lblSubsExt)
 		txtSubsExt = gtk.Entry()
@@ -85,27 +97,34 @@ class subMan():
 		txtSubsExt.connect('changed', self.subsExtsChanged)
 		self.txtSubsExt = txtSubsExt
 		hBox.pack_start(txtSubsExt)
+		# The add subtitles to current stream button.
 		btnAddSub = gtk.Button(_("Add subtitles to current stream"))
 		img = gtk.Image()
 		img.set_from_stock(gtk.STOCK_ADD, gtk.ICON_SIZE_BUTTON)
 		btnAddSub.set_image(img)
 		btnAddSub.connect('clicked', self.addSubs)
 		vBox.pack_start(btnAddSub)
+		# The close button.
 		btnClose = gtk.Button('gtk-close')
 		btnClose.set_use_stock(True)
 		btnClose.connect('clicked', self.destroy)
 		vBox.pack_start(btnClose)
+		# Do it all.
 		self.getCfg()
 		window.show_all()
 
 
 def trySubs(file):
 	# Trys to automatically set the subtitle track for a file.
+	# Get rid of the file:// at the start (if it's there).
 	if ('file://' in file): file = file[7:]
+	# Rip off the files extension.
 	(root, x) = os.path.splitext(file)
 	for ext in cfg.getStr('video/autosubexts').split(','):
+		# For all the extensions in the extensions list.
 		subPath = '%s.%s' % (root, ext)
 		if os.path.exists(subPath):
+			# If the subtitle file exists DO IT!
 			player.player.set_property('suburi', useful.filenameToUri(subPath))
 			print _("Found subtitles stream %s" % subPath)
 			return True
