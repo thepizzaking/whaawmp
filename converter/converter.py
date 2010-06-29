d882b1ed010d3c07e3d967c693fa0c319047d011
Jeffrey Bailes <thepizzaking@gmail.com>
2010-06-29 19:40:17 +1000

    Add copyright notice.

--- converter/converter.py
+++ converter/converter.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  Whaaw! Media Converter for transcoding media files.
+#  Copyright © 2007-2010, Jeff Bailes <thepizzaking@gmail.com>
+#       This file is part of Whaaw! Media Player (whaawmp)
+#
+#       whaawmp is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the Licence, or
+#       (at your option) any later version.
+#       
+#       whaawmp is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#		The Whaaw! Media Player project hereby grants permission for non-GPL
+#		compatible GStreamer plugins to be used and distributed together with
+#       GStreamer and Whaaw! Media Player. This permission is above and beyond
+#		the permissions granted by the GPL licence by which Whaaw! Media Player
+#		is covered. (See COPYING file for more details)
+
 
 from __future__ import print_function
 from __future__ import division
