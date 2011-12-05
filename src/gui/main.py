1b862e4d89a2e9e72ca28f59b690b117a8e08b00
Jeffrey Bailes <thepizzaking@gmail.com>
2011-12-05 22:19:16 +1100

    Continuously opening/closing the queue would shrink the video window to 0.
    Fixed this.

--- src/gui/main.py
+++ src/gui/main.py
@@ -765,11 +765,13 @@ class mainWindow:
 		ctrls = self.wTree.get_object("hboxAdvCtrls")
 		if (toShow):
 			ctrls.show()
+			#hboxHeight = ctrls.get_allocation().height
+			#useful.modifyWinHeight(self.mainWindow, hboxHeight)
 		else:
 			ctrls.hide()
 			# If we're closing it, we should shrink the main window too.
-			hboxHeight = ctrls.get_allocation().height
-			useful.modifyWinHeight(self.mainWindow, - (hboxHeight))
+			#hboxHeight = ctrls.get_allocation().height
+			#useful.modifyWinHeight(self.mainWindow, - (hboxHeight))
 	
 	def connectLinkHooks(self):
 		## Make hooks for opening URLs and e-mails.
