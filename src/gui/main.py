06f4740f26e05b8b95169145d47900f5873740ec
George Vlahavas <vlahavas~at~gmail~dot~com>
2012-05-11 16:16:57 +0300

    Only show the file basename in the now playing label, instead of showing the entire path. The path sometimes could be so lengthy, that the actual filename was truncated.

--- src/gui/main.py
+++ src/gui/main.py
@@ -420,7 +420,7 @@ class mainWindow:
 			# If no file is to be played, set the URI to None, and the file to ""
 			file = ""
 		# Set the now playing label to the file to be played.
-		self.nowPlyLbl.set_label(urllib.url2pathname(file))
+		self.nowPlyLbl.set_label(os.path.basename(urllib.url2pathname(file)))
 		if (os.path.exists(file) or '://' in file):
 			# If it's not already a uri, make it one.
 			file = useful.filenameToUri(file)
